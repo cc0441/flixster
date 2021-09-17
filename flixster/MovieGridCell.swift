a35de3f048d127038cc7c697a2320010e1e83e46
cc0441 <61059633+cc0441@users.noreply.github.com>
2021-09-16 23:57:13 -0400

    Initial Commit

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by cheng cheng on 9/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
